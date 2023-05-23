d5132ac9eea0398fe7acff319857565ba15e3d55
kuzu_no_takumi <jackson.com.24@gmail.com>
2023-05-23 22:29:48 +0900

    has_many,belongs_to追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+    belongs_to :user
+# 1対多の、多のtweetモデルに単数形のモデル名を記載
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+          
+  has_many :tweets
+# 1対多の、1のuserモデルに複数系のモデル名を記載
 end
